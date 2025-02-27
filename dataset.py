3cae05b9b382ef52ef83565b9277ecde5d70c663
Ruslon1 <84863005+Ruslon1@users.noreply.github.com>
2025-02-28 00:08:22 +0500

    train loader

--- dataset.py
+++ dataset.py
@@ -1,3 +1,11 @@
 import torchvision
+import torch
+import torchvision.transforms as transforms
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
 
 train_dataset = torchvision.datasets.CIFAR10(root='./CIFAR-10', train=True, download=True)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
