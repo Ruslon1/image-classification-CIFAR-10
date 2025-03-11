b60bb1a492db322ff585c432a8f0436cfa30b4bd
Ruslon1 <rrakhimov06@gmail.com>
2025-03-11 18:02:07 +0500

    Increase nn size

--- scripts/cnn.py
+++ scripts/cnn.py
@@ -5,14 +5,7 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout(0.25),
-
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.Conv2d(3, 128, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.Conv2d(128, 128, kernel_size=3, padding=1),
             nn.ReLU(),
@@ -24,12 +17,19 @@ class CNN(nn.Module):
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.25),
+
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout(0.25)
         )
 
         self.fc_layers = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(256 * 4 * 4, 512),
+            nn.Linear(512 * 4 * 4, 512),
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(512, 128),
